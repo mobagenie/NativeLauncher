0e96fc5d81ae9ce421e77fdc130862325e19f53e
Moba Genie <milengame4270@gmail.com>
2026-02-08 14:18:09 +0700

    rebuilding site Sun Feb  8 14:18:09 WIB 2026

--- app/src/main/java/com/nativelauncher/app/MainActivity.java
+++ app/src/main/java/com/nativelauncher/app/MainActivity.java
@@ -1,4 +1,4 @@
-package com.nativelauncher;
+package com.nativelauncher.app;
 
 import android.app.NativeActivity;
 import android.content.Intent;
@@ -6,7 +6,7 @@ import android.util.Log;
 
 public class MainActivity extends NativeActivity {
 
-    // Method dipanggil dari C++ via JNI
+    // Method ini akan dipanggil dari C++ via JNI
     public void launchApp(String packageName) {
         try {
             Intent intent = getPackageManager().getLaunchIntentForPackage(packageName);
@@ -20,27 +20,3 @@ public class MainActivity extends NativeActivity {
         }
     }
 }
-
-package com.nativelauncher;
-
-import android.app.NativeActivity;
-import android.content.Intent;
-import android.util.Log;
-
-public class MainActivity extends NativeActivity {
-
-    // Method dipanggil dari C++ via JNI
-    public void launchApp(String packageName) {
-        try {
-            Intent intent = getPackageManager().getLaunchIntentForPackage(packageName);
-            if (intent != null) {
-                startActivity(intent);
-            } else {
-                Log.i("LiteLauncher", "App not installed: " + packageName);
-            }
-        } catch (Exception e) {
-            Log.e("LiteLauncher", "Launch failed", e);
-        }
-    }
-}
-
